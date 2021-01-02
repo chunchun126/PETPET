a71967fe282bc4dda332c38a902dc9fa81ca4b61
chunchun126 <chun780126@gmail.com>
2021-01-02 17:19:19 +0800

    Update 2021-01-02T09:19:10.842Z

---
+++ assets/js/all.js
@@ -0,0 +1,99 @@
+"use strict";
+
+AOS.init({
+  // Global settings:
+  disable: false,
+  // accepts following values: 'phone', 'tablet', 'mobile', boolean, expression or function
+  startEvent: 'DOMContentLoaded',
+  // name of the event dispatched on the document, that AOS should initialize on
+  initClassName: 'aos-init',
+  // class applied after initialization
+  animatedClassName: 'aos-animate',
+  // class applied on animation
+  useClassNames: false,
+  // if true, will add content of `data-aos` as classes on scroll
+  disableMutationObserver: false,
+  // disables automatic mutations' detections (advanced)
+  debounceDelay: 50,
+  // the delay on debounce used while resizing window (advanced)
+  throttleDelay: 99,
+  // the delay on throttle used while scrolling the page (advanced)
+  // Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
+  offset: 120,
+  // offset (in px) from the original trigger point
+  delay: 0,
+  // values from 0 to 3000, with step 50ms
+  duration: 800,
+  // values from 0 to 3000, with step 50ms
+  easing: 'ease',
+  // default easing for AOS animations
+  once: true,
+  // whether animation should happen only once - while scrolling down
+  mirror: false,
+  // whether elements should animate out while scrolling past them
+  anchorPlacement: 'top-bottom' // defines which position of the element regarding to window should trigger the animation
+
+});
+$(document).on("click", ".link-arrow", function (evt) {
+  evt.preventDefault();
+  var target = $(this).attr("href");
+  $('html,body').animate({
+    scrollTop: $(target).offset().top
+  }, 500);
+}); // Swiper
+
+var swiper = new Swiper('.swiper-container', {
+  slidesPerView: 3,
+  spaceBetween: 30,
+  loop: true,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev'
+  },
+  autoplay: {
+    delay: 5000,
+    disableOnInteraction: false
+  }
+});
+$(document).ready(function () {
+  // navbar
+  $(window).scroll(function () {
+    if ($(window).scrollTop() <= 0) {
+      $('.navbar').removeClass('shadow-sm');
+    } else {
+      $('.navbar').addClass('shadow-sm');
+    }
+  }); // carousel
+
+  $(".carousel__item a").click(function (event) {
+    $(".carousel__item--active").attr("src", $(this).attr('href'));
+    event.preventDefault(); // 避免轉址，也可使用 return false;
+  }); // modal
+
+  $('.modal').css('overflow-y', 'auto');
+}); // Example starter JavaScript for disabling form submissions if there are invalid fields
+
+(function () {
+  'use strict';
+
+  window.addEventListener('load', function () {
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    var forms = document.getElementsByClassName('needs-validation'); // Loop over them and prevent submission
+
+    var validation = Array.prototype.filter.call(forms, function (form) {
+      form.addEventListener('submit', function (event) {
+        if (form.checkValidity() === false) {
+          event.preventDefault();
+          event.stopPropagation();
+        }
+
+        form.classList.add('was-validated');
+      }, false);
+    });
+  }, false);
+})();
+//# sourceMappingURL=all.js.map
